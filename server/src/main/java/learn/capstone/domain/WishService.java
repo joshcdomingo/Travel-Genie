02bef5ac8768fcf33d973635cbdd337f61427981
Audrey Borges <audreyborges7@gmail.com>
2022-10-25 10:10:43 -0500

    completed Wish Service

--- server/src/main/java/learn/capstone/domain/WishService.java
+++ server/src/main/java/learn/capstone/domain/WishService.java
@@ -11,16 +11,64 @@ public class WishService {
     public WishService(WishFileRepository repository) {
         this.repository = repository;
     }
-    Wish findById(int wishId) {
-        return repository.findById(wishId);
+
+    public Result<Wish> findById(int wishId) {
+        Result<Wish> result = new Result<>();
+
+        if (wishId <= 0) {
+            result.addMessage("Wish Id cannot be less than or equal to zero", ResultType.INVALID);
+            return result;
+        }
+
+        Wish wish = repository.findById(wishId);
+
+        if (wish == null) {
+            result.addMessage("Wish Id was not found.", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        result.setPayload(wish);
+        return result;
     }
 
-    Wish add(Wish wish) {
-        return repository.add(wish);
+    public Result<Wish> add(Wish wish) {
+        Result<Wish> result = validate(wish);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (wish.getAppUserId() != 0) {
+            result.addMessage("AppUser Id cannot be less than or equal to zero", ResultType.INVALID);
+            return result;
+        }
+
+        if (wish.getCityId() != 0) {
+            result.addMessage("City Id cannot be less than or equal to zero", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        wish = repository.add(wish);
+        result.setPayload(wish);
+        return result;
     }
 
-    Wish findByAppUserId(int app_userId) {
-        return repository.findByAppUserId(app_userId);
+    public Result<Wish> findByAppUserId(int app_userId) {
+        Result<Wish> result = new Result<>();
+
+        if (app_userId <= 0) {
+            result.addMessage("AppUser Id cannot be less than or equal to zero", ResultType.INVALID);
+            return result;
+        }
+
+        Wish wish = repository.findByAppUserId(app_userId);
+
+        if (wish == null) {
+            result.addMessage("Wish Id was not found.", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        result.setPayload(wish);
+        return result;
     }
 
     List<Wish> findAll() {
@@ -29,5 +77,20 @@ public class WishService {
 
     boolean deleteById(int agentId) {
         return repository.deleteById(agentId);
+    }
+
+    private Result<Wish> validate(Wish wish) {
+        Result<Wish> result = new Result<>();
+        if (wish == null) {
+            result.addMessage("Wish Id was not found.", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        if (wish.getWishId() != 0) {
+            result.addMessage("Wish Id cannot be less than or equal to zero", ResultType.INVALID);
+            return result;
+        }
+
+        return result;
     }
 }
