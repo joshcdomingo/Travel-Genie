5602bddde36b1f8eed0083598151481de86047c2
Audrey Borges <audreyborges7@gmail.com>
2022-10-25 09:22:06 -0500

    created Entertainment Service

--- server/src/main/java/learn/capstone/domain/EntertainmentService.java
+++ server/src/main/java/learn/capstone/domain/EntertainmentService.java
@@ -1,4 +1,26 @@
 package learn.capstone.domain;
 
+import learn.capstone.data.EntertainmentFileRepository;
+import learn.capstone.models.Entertainment;
+
+import java.util.List;
+
 public class EntertainmentService {
+    private final EntertainmentFileRepository repository;
+
+    public EntertainmentService(EntertainmentFileRepository repository) {
+        this.repository = repository;
+    }
+
+    Entertainment findById(int entertainmentId)  {
+        return repository.findById(entertainmentId);
+    }
+
+    List<Entertainment> findByCityId(int cityId) {
+        return repository.findByCityId(cityId);
+    }
+
+    List<Entertainment> findAll()  {
+        return repository.findAll();
+    }
 }
