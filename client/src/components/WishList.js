75ccf2022d9d34019eb0a2c6743ce33dc422a8b5
s-troutmant <star_t@starsdesign.us>
2022-10-31 10:48:15 -0400

    fixed the delete confirmation

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
 
 
-
 function WishList() {
  
   const auth = useContext(AuthContext);
@@ -35,13 +34,12 @@ function WishList() {
       (wish) => wish.wishId === wish_id
     );
 
-    if (
-      window.confirm(
-        `Delete wish ${wish.city} ${wish.country}?`
-      )
-    ) {
+    if (window.confirm(`Delete wish ${wish.cityName} ${wish.countryName}?`)) {
       const init = {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${auth.user.token}`,
+        }  
       };
 
       fetch(`${endpoint}/${wish_id}`, init)
@@ -57,44 +55,41 @@ function WishList() {
   };
 
   return (
-    <>
-<div className="container">
-  <h2>Wish List</h2>
-  <Link className="btn btn-primary" to="/WishForm">
-    Add Wish
-  </Link>
-  <table>
-    <thead>
-      <tr>
-        <th>City</th>
-        <th>Country</th>
-        <th>Scenery</th>
-        <th>Entertainment</th>
-        <th>&nbsp;</th>
-      </tr>
-    </thead>
-    <tbody>
-      {wishes.map((wish) => (
-        <tr key={wish.wishId}>
-          <td>{wish.cityName}</td>
-          <td>{wish.countryName}</td>
-          <td>{wish.scenery}</td>
-          <td>{wish.entertainmentName}</td>
-          <td className="buttonContainer">
-            <button
-              className="btn btn-danger"
-              onClick={() => handleDeleteWish(wish.wishId)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-  </div>
-</>
-);
+    <main>
+      <div className="container">
+      <h2>Wish List</h2>
+        <Link className="btn btn-primary" to="/WishForm">
+          Add Wish
+        </Link>
+        <table>
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Country</th>
+              <th>Scenery</th>
+              <th>Entertainment</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wishes.map((wish) => (
+              <tr key={wish.wishId}>
+                <td>{wish.cityName}</td>
+                <td>{wish.countryName}</td>
+                <td>{wish.scenery}</td>
+                <td>{wish.entertainmentName}</td>
+                <td className="buttonContainer">
+                  <button className="btn btn-danger" onClick={() => handleDeleteWish(wish.wishId)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+           ))}
+          </tbody>
+       </table>
+    </div>
+  </main>
+  );
 }
 
 export default WishList;
