afc73fd63c95c3e0828e16fedb51c214b08852af
Audrey Borges <audreyborges7@gmail.com>
2022-10-31 13:01:22 -0500

    designed welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -11,9 +11,10 @@ function Welcome() {
     <main>
       {!auth.user ? (
         <div className="welcome_page">
+          <img id="welcomeDesert" src="/images/welcomeDesert.jpeg " alt="daytime cartoon desert with camel and sun"></img>
           <LogoAnimation />
-            <Link className="btn btn-info" to="/registration">Register</Link>
-            <Link className="btn btn-info" to="/login">Login</Link>
+            <Link className="btn btn-light" to="/registration">Register</Link>
+            <Link className="btn btn-dark" to="/login">Login</Link>
         </div> ) : (<div></div>)}
     </main>    
   );
