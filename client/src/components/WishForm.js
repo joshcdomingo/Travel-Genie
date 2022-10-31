3c421bfa0ca6a8e656658bc3f5659c8e0c88f34f
s-troutmant <star_t@starsdesign.us>
2022-10-31 14:52:59 -0400

    changed wishForm to condition render

--- client/src/components/WishForm.js
+++ client/src/components/WishForm.js
@@ -15,11 +15,10 @@ function WishForm() {
     const endpoint = `http://localhost:8080/api/travelgenie/wish/match`;
     const [errs, setErrs] = useState({});
     const [matches, setMatches] = useState([]);
+    const [submitted, setSubmitted] = useState(false);
     const [wish, setWish] = useState(WISH_DEFAULT);
     const history = useHistory();
-    
-    
-    
+        
     const getMatches = () => {
         
         const init = {
@@ -37,8 +36,6 @@ function WishForm() {
           .catch((error) => console.log(error));
     };
 
-
-
     function handleChange(evt) {
         const nextChoice = { ...wish };
         if (evt.target.name === "kidFriendly") {
@@ -53,123 +50,115 @@ function WishForm() {
     function handleSubmit(evt) {
         evt.preventDefault();
     
-            getMatches();
-     
-
+        setSubmitted(true);
+            
+        getMatches();
     }
 
-    const handleAddWish = async () => {
-        const newWish = {
-            appUserId: auth.user.userId,
-            cityName: String(matches.map(m => m.cityName)),
-            countryName: String(matches.map(m => m.countryName)),
-            scenery: String(matches.map(m => m.scenery)),
-            entertainmentName: String(matches.map(m => m.entertainmentName)),
-            activityLevel: String(matches.map(m => m.activityLevel)),
-            priceRange: String(matches.map(m => m.priceRange)),
-            kidFriendly: Boolean(matches.map(m => m.kidFriendly))
-        };
-        
-        if (window.confirm("Add this wish to your checklist?")) {
-        const init = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${auth.user.token}`
-          },
-          body: JSON.stringify(newWish),
-        };
-        fetch("http://localhost:8080/api/travelgenie/wish", init)
-          .then((response) => {
-            if (response.status === 201 || response.status === 400) {
-              return response.json();
-            } else {
-              return Promise.reject(`Unexpected status code: ${response.status}`);
-            }
-          })
-          .then((data) => {
-            if (data.wishId) {
-              // happy path
-              resetState();
-              // navigate back home "/"
-            } else {
-              setErrs(data);
-            }
-          })
-          .catch((error) => console.log(error));
+    const handleAddWish = async (wish) => {
 
-          history.push("/WishList");
+        wish.appUserId = auth.user.userId;
+        
+        if (window.confirm("Add this wish to your wish list?")) {
+            const init = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${auth.user.token}`
+                },
+                body: JSON.stringify(wish),
+            };
+        
+            fetch("http://localhost:8080/api/travelgenie/wish", init)
+            .then((response) => {
+                if (response.status === 201 || response.status === 400) {
+                    return response.json();
+                } else {
+                return Promise.reject(`Unexpected status code: ${response.status}`);
+                }
+            })
+            .then((data) => {
+                if (data.wishId) {
+                    history.push("/WishList");
+                } else {
+                    setErrs(data);
+                }
+            })
+            .catch((error) => console.log(error));
         }
         else{
             history.push("/Home");
         }
+    };
 
-
-      };
-
-      const resetState = () => {
+    const resetState = () => {
         setWish(WISH_DEFAULT);
         setErrs([]);
-      };
+        setSubmitted(false);
+    };
       
-
-
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Wish Form</h2>
-            {errs.length > 0 && <div>
-                <ul>
-                    {errs.map(e => <li key={e}>{e}</li>)}
-                </ul>
-            </div>}
-            <div>
-                <label htmlFor="scenery">Scenery: </label>
-                <select id="scenery" name="scenery"
-                    value={wish.scenery} onChange={handleChange}>
-                    <option>METROPOLITAN</option>
-                    <option>BEACH</option>
-                    <option>MOUNTAIN</option>
-                    <option>DESSERT</option>
-                    <option>SNOW</option>
-
-                </select>
+        <main>
+        {!submitted ?
+            <form onSubmit={handleSubmit}>
+                <h2>Wish Form</h2>
                 <div className="form-group">
-                <label htmlFor="activityLevel">Activity Level: </label>
-                <select id="activityLevel" name="activityLevel"
-                    value={wish.activityLevel} onChange={handleChange}>
-                    <option>LOW</option>
-                    <option>MEDIUM</option>
-                    <option>HIGH</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor="kidFriendly">KidFriendly?</label>
-                <input type="checkbox" id="kidFriendly" name="kidFriendly"
-                    checked={wish.kidFriendly} onChange={handleChange}></input>
-            </div>
-            <div className="form-group">
-            <label htmlFor="priceRange">Price Range: </label>
-                <select id="priceRange" name="priceRange"
-                    value={wish.priceRange} onChange={handleChange}>
-                    <option>$</option>
-                    <option>$$</option>
-                    <option>$$$</option>
-                </select>
-            </div>
-            </div>
-            <div>
-                <button type="submit" className="button" onClick={() => getMatches()}>Find Destination</button>
-                <Link to="/Home" className="btn btn-danger">Cancel</Link>
-                <h1> {matches.map(m => m.cityName)}</h1>
-                <h1> {matches.map(m => m.entertainmentName)}</h1> 
-                <button
-              className="btn btn-success"
-              onClick={() => handleAddWish(wish.wishId)}
-            >
-              Add
-            </button>
-            </div>
-        </form>
+                    <label htmlFor="scenery">Scenery: </label>
+                    <select id="scenery" name="scenery" value={wish.scenery} onChange={handleChange}>
+                        <option>METROPOLITAN</option>
+                        <option>BEACH</option>
+                        <option>MOUNTAIN</option>
+                        <option>DESSERT</option>
+                        <option>SNOW</option>
+                    </select>
+                    <label htmlFor="activityLevel">Activity Level: </label>
+                    <select id="activityLevel" name="activityLevel" value={wish.activityLevel} onChange={handleChange}>
+                        <option>LOW</option>
+                        <option>MEDIUM</option>
+                        <option>HIGH</option>
+                    </select>
+                    <label htmlFor="kidFriendly">KidFriendly?</label>
+                    <input type="checkbox" id="kidFriendly" name="kidFriendly" checked={wish.kidFriendly} onChange={handleChange}></input>
+                    <label htmlFor="priceRange">Price Range: </label>
+                        <select id="priceRange" name="priceRange" value={wish.priceRange} onChange={handleChange}>
+                        <option>$</option>
+                        <option>$$</option>
+                        <option>$$$</option>
+                    </select>
+                    <button type="submit" className="btn btn-primary">Find Destination</button>
+                    <Link to="/Home" className="btn btn-danger">Cancel</Link>
+                </div>
+            </form> 
+            : 
+            <div className="search-result">    
+                <table>
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Country</th>
+                            <th>Scenery</th>
+                            <th>Entertainment</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {matches.map((wish) => (
+                            <tr key={wish.wishId}>
+                                <td>{wish.cityName}</td>
+                                <td>{wish.countryName}</td>
+                                <td>{wish.scenery}</td>
+                                <td>{wish.entertainmentName}</td>
+                                <td className="buttonContainer">
+                                    <button className="btn btn-warning" onClick={() => handleAddWish(wish)}>
+                                        Add
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>}
+        </main> 
     );
 }
 
